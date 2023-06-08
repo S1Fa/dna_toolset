c0520774f9f9c33ef0999722a9d7626d9d8c7161
sifa <sifa17@qq.com>
2023-06-08 21:10:29 +0800

    GC Content

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -2,9 +2,8 @@
 import collections
 from structures import DNA_ReverseComplement, Nucleotides
 
-
-# Check the sequence to make sure it is a DNA string
 def validateSeq(dna_seq):
+    """Check the sequence to make sure it is a DNA string"""
     temSeq = dna_seq.upper()
     for nuc in temSeq:
         if nuc not in Nucleotides:
@@ -23,5 +22,22 @@ def transcription(seq):
     return seq.replace("T", "U")
 
 def reverse_complement(seq):
-    """Swapping adenine with thymine and guanine with cytosine. Reversing newly generated string"""
-    return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
+    """Swapping adenine with thymine and guanine with cytosine.
+    Reversing newly generated string"""
+    # return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
+
+    # Pythonic approach. A little bit faster solution.
+    mapping = str.maketrans("ATCG", "TAGC")
+    return seq.translate(mapping)[::-1]
+
+def gc_content(seq):
+    """GC content in a DNA/RNA sequence (percentage)"""
+    return round((seq.count('C') + seq.count("G")) / len(seq) * 100, 6)
+
+def gc_content_subsec(seq, k=20):
+    """GC content in a DNA/RNA sub-sequence length k. k=20 by default"""
+    res = []
+    for i in range(0, len(seq)-k+1, k):
+        res.append(gc_content(seq[i: i+k]))
+    
+    return res
--- main.py
+++ main.py
@@ -8,13 +8,30 @@ randDNAStr = ''.join([random.choice(Nucleotides) for _ in range(50)])
 
 dna_seq = validateSeq(randDNAStr)
 
-print(f'Sequence: {colored(dna_seq)}')
-print(f'[1] + Sequence Length: {len(dna_seq)}')
-print(colored(f'[2] + Nucleotides Frequency: {countNucFrequency(dna_seq)}'))
+# === colorize ===
 
-print(f'[3] + DNA->RNA transcription {colored(transcription(dna_seq))}')
+# print(f'Sequence: {colored(dna_seq)}')
+# print(f'[1] + Sequence Length: {len(dna_seq)}')
+# print(colored(f'[2] + Nucleotides Frequency: {countNucFrequency(dna_seq)}'))
+
+# print(f'[3] + DNA->RNA transcription {colored(transcription(dna_seq))}')
+
+# print(f'[4] + DNA Complement:')
+# print(f"5' {colored(dna_seq)} 3'")
+# print(f"   {'|'*len(dna_seq)}")
+# print(f"3' {colored(reverse_complement(dna_seq)[::-1])} 5'")
+
+print(f'Sequence: {dna_seq}')
+print(f'[1] + Sequence Length: {len(dna_seq)}')
+print(f'[2] + Nucleotides Frequency: {countNucFrequency(dna_seq)}')
+
+print(f'[3] + DNA->RNA transcription {transcription(dna_seq)}')
 
 print(f'[4] + DNA Complement:')
-print(f"5' {colored(dna_seq)} 3'")
+print(f"5' {dna_seq} 3'")
 print(f"   {'|'*len(dna_seq)}")
-print(f"3' {colored(reverse_complement(dna_seq)[::-1])} 5'")
+print(f"3' {reverse_complement(dna_seq)[::-1]} 5'")
+
+print(f'[5] + GC content: {gc_content(dna_seq)}%')
+
+print(f'[6] + GC content in subsection k=5: {gc_content_subsec(dna_seq, k=5)}')
